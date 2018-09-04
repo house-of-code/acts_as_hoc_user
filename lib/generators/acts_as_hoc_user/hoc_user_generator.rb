197b3dff783da8f546f4c10909c0e05a55367a11
Gert Lavsen <gert@houseofcode.io>
2018-09-04 09:37:09 +0200

    fixed generator

--- lib/generators/acts_as_hoc_user/hoc_user_generator.rb
+++ lib/generators/acts_as_hoc_user/hoc_user_generator.rb
@@ -1,36 +1,35 @@
 require 'rails/generators/active_record'
 module ActsAsHocUser
-  module Generators
-    class HocUserGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
+  #module Generators
+    class HocUserGenerator < ActiveRecord::Generators::Base
+
       desc "Create a HocUser model and migrations " +
       "The NAME argument is the name of your model, and the following " +
-      "arguments are the fields to add. Eg. hoc_user user name:string"
-      argument :name, :required => true, :type => :string, :desc => "name of the model.",
-      argument :fields, :required => false, :type => :array, :desc => "The fields to app.",
+      "arguments are the fields to add. Eg. acts_as_hoc_user:hoc_user user name:string"
+
+      argument :fields, :required => false, :type => :array, :desc => "The fields to add.",
       :banner => "name:string age:integer ..."
 
       def self.source_root
         @source_root ||= File.expand_path('../templates', __FILE__)
       end
 
-      def copy_initializer_file
-        template "initializer.rb", "config/initializers/acts_as_hoc_user.rb"
-      end
 
       def generate_migration
+        template "initializer.rb", "config/initializers/acts_as_hoc_user.rb"
         template "hoc_user.rb.erb", "app/models/#{model_name}.rb"
         migration_template("create_hoc_user.rb.erb",
           "db/migrate/#{migration_file_name}",
           migration_version: migration_version)
       end
-
+      
       def model_name
         name.underscore
       end
 
       def migration_colums
-        fields.map { |field| "t.#{field.split(":").last} :#{field.split(":").first}" }
+        return fields.map { |field| "t.#{field.split(":").last} :#{field.split(":").first}" } unless fields.nil?
+        return []
       end
 
       def model_class_name
@@ -55,5 +54,5 @@ module ActsAsHocUser
         end
       end
     end
-  end
+  #end
 end

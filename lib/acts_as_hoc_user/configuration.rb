53daa246792ff8e41d6c362399f732a6f7686725
Gert Lavsen <gert@houseofcode.io>
2018-09-10 13:08:14 +0200

    added acts_as_hoc_user_secret to configuration

--- lib/acts_as_hoc_user/configuration.rb
+++ lib/acts_as_hoc_user/configuration.rb
@@ -1,6 +1,6 @@
 module ActsAsHocUser
   class Configuration
-    attr_accessor :min_password_length
+    attr_accessor :min_password_length, :acts_as_hoc_user_secret
     def initialize
     end
   end

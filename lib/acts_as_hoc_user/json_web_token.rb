a58f0b20a1dc2b7c8e100c855631fa938f04dc57
Gert Lavsen <gert@houseofcode.io>
2018-09-10 13:12:32 +0200

    use right configuration key and bumped version

--- lib/acts_as_hoc_user/json_web_token.rb
+++ lib/acts_as_hoc_user/json_web_token.rb
@@ -4,11 +4,11 @@ module ActsAsHocUser
     class << self
       def encode(payload, exp = 48.hours.from_now)
         payload[:exp] = exp.to_i
-        JWT.encode(payload, Rails.application.config.acts_as_hoc_user_secret)
+        JWT.encode(payload, ActsAsHocUser.configuration.acts_as_hoc_user_secret)
       end
 
       def decode(token)
-        body = JWT.decode(token, Rails.application.config.acts_as_hoc_user_secret)[0]
+        body = JWT.decode(token, ActsAsHocUser.configuration.acts_as_hoc_user_secret)[0]
         HashWithIndifferentAccess.new body
       rescue
         nil
--- lib/acts_as_hoc_user/version.rb
+++ lib/acts_as_hoc_user/version.rb
@@ -1,3 +1,3 @@
 module ActsAsHocUser
-  VERSION = "1.0.7"
+  VERSION = "1.0.8"
 end
